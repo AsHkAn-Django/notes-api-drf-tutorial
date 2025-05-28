b0fb3e78c71acfe32ff2ede7429f19ba0c1dc54c
AsHkaN <ashkan_ahrari@yahoo.com>
2025-05-28 12:50:48 +0300

    model and serializer added

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=140)
+    body = models.TextField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='notes', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
